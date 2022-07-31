8ebeff587ffa284e66863a81eb8fad65116367be
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-07-31 18:12:22 -0400

    Modelo, migration e controlador para Article

---
+++ database/migrations/2022_07_31_180839_create_articles_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use App\Models\Page;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+
+            $table->string("title")->fulltext()->nullable(false);
+            $table->string("description")->fulltext()->nullable(false);
+            $table->string("cover")->nullable();
+            $table->string("lang", 5)->nullable(false)->default(config("app.locale"));
+            $table->string("content")->nullable(true);
+            $table->integer("protection")->nullable(false)->default(Page::PROTECTION_AUTHOR);
+            $table->boolean("follow")->nullable(false)->default(true);
+
+            $table->string("status")->nullable(false)->default("draft");
+            $table->timestamp("published_at")->nullable(true);
+            $table->timestamp("scheduled_to")->nullable(true);
+
+            // FOREIGN KEYS
+            $table->foreignId("author")->unsigned()->nullable(true);
+            $table->foreignId("slug")->unsigned()->nullable(false);
+
+            $table->foreign("author")->references("id")->on("users")->nullOnDelete();
+            $table->foreign("slug")->references("id")->on("slugs")->restrictOnDelete();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
