f7c055e268f617ad9b8982a605ceaae3f7604196
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-08-11 13:57:19 -0400

    .

--- routes/web.php
+++ routes/web.php
@@ -139,4 +139,4 @@ Route::get("/home", function () {
 });
 Route::get("/termos-e-condicoes", [FrontController::class, "termsAndConditions"])->name("front.termsAndConditions");
 Route::get("/p/{slug}", [FrontController::class, "dinamicPage"])->name("front.dinamicPage");
-Route::get("/builder", [FrontController::class, "builder"]);
+Route::get("/builder/builder", [FrontController::class, "builder"]);
