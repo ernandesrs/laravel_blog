970213a70418a240718957e99c73d3096746f9b7
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-08-15 08:11:24 -0400

    Política de comentários

--- app/Http/Controllers/Front/FrontController.php
+++ app/Http/Controllers/Front/FrontController.php
@@ -125,6 +125,83 @@ class FrontController extends Controller
         $useTerms->slug_id = $useTermsSlug->id;
         $useTerms->save();
 
+        /**
+         * comment terms page create
+         */
+
+        $commentTermsSlug = new Slug();
+        $commentTermsSlug->set("politica-de-comentarios", config("app.locale"));
+        $commentTermsSlug->save();
+
+        $commentTerms = new Page();
+        $commentTerms->title = "Política de comentários";
+        $commentTerms->description = "Uma descrição legal para esta página de política de comentários";
+        $commentTerms->content_type = Page::CONTENT_TYPE_TEXT;
+        $commentTerms->content = $commentTerms->getContent(["content_type" => Page::CONTENT_TYPE_TEXT, "content" => '<h2><strong>Política de comentários</strong></h2>
+        <p>Apreciamos discussões e diferentes pontos de vista, mas o mais 
+        importante é que valorizamos a discussões respeitosas e construtivas
+        com outros os autores e leitores. </p>
+        <p>Temos tolerância zero para 
+        comentários que acreditamos que possam ser considerados inapropriados ou
+         ofensivos, que exibam comportamento semelhante a trolls e que não seja relacionado ao conteúdo abordado na página.</p>
+        <p>Os comentaristas podem 
+        sinalizar comentários potencialmente inadequados para revisão, por 
+        preocupação com outras pessoas ou devido a ofensa pessoal. </p>
+        <p>Você pode sinalizar um comentário por meio do menu ao lado de um comentário potencialmente 
+        ofensivo ou nos enviar um e-mail por meio de nossa página de contato. </p>
+        <h2><strong>Como comentar: </strong></h2>
+        <p>Nossos comentários são registrados e moderados plataforma de terceiros(<a href="https://disqus.com/" target="_blank">Disqus</a>), que exige que os usuários configurem uma conta. </p>
+        <p>Você pode registrar um perfil Disqus diretamente no Disqus ou pode facilmente realizar um cadastro rápido com suas contas do Facebook, Twitter ou Google. </p>
+        <p>Ao contrário das primeiras políticas do Facebook e do Google, o Disqus não exige que você use seu nome real. </p>
+        <p>No entanto, preferimos que os 
+        comentaristas usem seu nome real porque eles são mais propensos a se 
+        comportar de forma mais madura, em vez de se soltarem enquanto se 
+        escondem atrás de um pseudônimo.</p><h2><strong>Vamos manter um relacionamento saudável:</strong></h2>
+        <p>Discussões e debates são 
+        bem-vindos, mas comentários que não tenha nada haver com o conteúdo da página, que contenha palavras de baixo calão, ofensas ao autor ou outros leitores não serão toleráveis. </p>
+        <p>Embora reconhecendo que 
+        haverá, às vezes, pontos de vista diferentes, nos reservamos o direito 
+        de advertir os comentaristas abertamente, excluir, editar ou modificar 
+        um tópico que viole nossas diretrizes e suspender ou banir infratores 
+        reincidentes. </p>
+        <p>Além disso, não toleraremos 
+        racismo, sexismo, homofobia ou outras formas de discurso de ódio ou 
+        contribuições que possam ser interpretadas como tal. </p>
+        <h2><strong>Clareza e intenção: </strong></h2>
+        <p>O tom da palavra escrita nem sempre é óbvio, então pare e pense duas vezes antes de publicar seu comentário. </p>
+        <p>"Gritos" – letras, palavras ou
+         frases em maiúsculas – e o uso de emojis, outros símbolos e linguagem 
+        imprópria são desaprovados porque são projetados para fazer um 
+        comentário se destacar dos outros. </p>
+        <p>O uso dessas e de técnicas 
+        semelhantes pode levar à exclusão, edição ou modificação do comentário a
+         critério da moderação.  Os infratores reincidentes 
+        podem enfrentar uma suspensão parcial ou um banimento permanente. </p>
+        <h2><strong>Imagens do usuário (avatares): </strong></h2>
+        <p>Imagens de mau gosto nos perfis de foto/imagem de usuário do Disqus (também conhecido como avatar) serão banidas. </p>
+        <p>Exemplos de conteúdo proibido 
+        incluem imagens sexualmente explícitas, sugestivas ou reveladoras;  
+        representações de violência ou sangue;  imagens brutas;  a celebração ou
+         representação de criminosos ou atividade criminosa;  propaganda. </p>
+        <h2><strong>Perfis alternativos: </strong></h2>
+        <p>Os comentaristas são 
+        solicitados a manter apenas um perfil, e nossos moderadores sinalizam os
+         endereços IP dos infratores reincidentes. </p>
+        <p>Infratores reincidentes com várias contas serão banidos da nossa seção de comentários. </p>
+        <h2><strong>Links: </strong></h2>
+        <p>Os links para todos os sites foram desativados para proteger nosso site e visitantes contra spam e malware. </p>
+        <p>Links para <em>páginas internas ao nosso site </em>são
+         permitidos, no entanto, comentários que incluam esses links serão 
+        inicialmente retidos, aguardando uma revisão antes de serem publicados. </p>
+        <h2><strong>Quem realiza as moderações? </strong></h2>
+        <p>O monitoramento e a moderação da discussão na seção de comentários do <em>nosso site </em>são feitos pelos autores e outros administradores do site. Os leitores podem contribuir sinalizando um comentário que viole nosas regras. </p>
+        <p><em>Estas diretrizes podem ser revisadas ou atualizadas a qualquer momento, sem aviso prévio. </em></p>']);
+        $commentTerms->protection = Page::PROTECTION_SYSTEM;
+        $commentTerms->status = Page::STATUS_PUBLISHED;
+        $commentTerms->user_id = $user->id;
+        $commentTerms->slug_id = $commentTermsSlug->id;
+        $commentTerms->save();
+
         echo "Builded";
         return;
     }
