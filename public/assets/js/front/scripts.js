e49ec97da9d675091538bc24976a2f78604e94cc
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-08-17 19:32:32 -0400

    production assets

--- public/assets/js/front/scripts.js
+++ public/assets/js/front/scripts.js
@@ -1,17 +1,1 @@
-$(function () {
-
-    $(".jsBtnToggler").on("click", function (e) {
-        e.preventDefault();
-
-        if ($(".sidebar").hasClass("show")) {
-            $(".sidebar").slideUp(125, function () {
-                $(this).removeClass("show").attr("style", "");
-            });
-        } else {
-            $(".sidebar").slideDown(250, function () {
-                $(this).addClass("show").attr("style", "");
-            });
-        }
-    });
-
-});
+$((function(){$(".jsBtnToggler").on("click",(function(s){s.preventDefault(),$(".sidebar").hasClass("show")?$(".sidebar").slideUp(125,(function(){$(this).removeClass("show").attr("style","")})):$(".sidebar").slideDown(250,(function(){$(this).addClass("show").attr("style","")}))}))}));
